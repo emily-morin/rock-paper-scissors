1018bc19ab5170acb8079217082389bd6759d1bc
emily_m <morin.emily@gmail.com>
2022-09-27 13:46:21 -0400

    Create function to play a single round of rock paper scissors

--- index.js
+++ index.js
@@ -3,7 +3,40 @@
 function getComputerChoice() {
     const choices = ['Rock', 'Paper', 'Scissors'];
     let randIndex = Math.floor(Math.random() * choices.length);
-    return choices[randIndex];
+    return choices[randIndex].toLowerCase();
+}
+// console.log(getComputerChoice());
+
+const playerSelection = prompt("Choose rock, paper, or scissors: ").toLowerCase();
+const computerSelection = getComputerChoice();
+
+// function to play a single round of RPS
+
+function playRound(playerSelection, computerSelection) {    
+
+// check if player entered valid choice
+    if (playerSelection === "" || playerSelection !== "rock" && playerSelection !== "paper" && playerSelection !== "scissors") {
+        console.log("Please enter a valid choice.");
+    }
+
+// computer wins
+    else if (computerSelection === "rock" && playerSelection === "scissors" || 
+    computerSelection === "paper" && playerSelection === "rock" ||
+    computerSelection === "scissors" && playerSelection === "paper") {
+        return `You Lose! ${computerSelection} beats ${playerSelection}`; 
+    }
+
+// player wins
+    else if (playerSelection === "rock" && computerSelection === "scissors" ||
+    playerSelection === "paper" && computerSelection === "rock" ||
+    playerSelection === "scissors" && computerSelection === "paper") {
+        return `You Win! ${playerSelection} beats ${computerSelection}`; 
+    }
+
+    else if (playerSelection === computerSelection) {
+        return "It's a tie!"
+    }
 }
 
-console.log(getComputerChoice());
+// test
+console.log(playRound(playerSelection, computerSelection));
